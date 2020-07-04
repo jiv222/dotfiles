bc4076cc2eca7a145aed2f040460857dca7cae31
blockiv <blockiv@protonmail.com>
2020-07-04 08:21:36 -0400

    changed albumart location

--- local/.local/bin/tools/songnotify.sh
+++ local/.local/bin/tools/songnotify.sh
@@ -8,7 +8,7 @@ kill -46 $(pidof dwmblocks)
 [[ "$PLAYER_EVENT" == "change" ]] || exit 1
 
 # Delete previous album art
-rm ~/.config/spotifyd/tmp/AlbumArt.png
+rm ~/.cache/AlbumArt.png
 
 # Get song metadata
 metadata="$(playerctl metadata --format "{{ artist }};{{ album }}")"
@@ -18,8 +18,8 @@ arist="${meta_array[0]}"
 album="${meta_array[0]}"
 
 # Get album art for icon
-sacad "${artist}" "${album}" 90 ~/.config/spotifyd/tmp/AlbumArt.png 
+sacad "${artist}" "${album}" 90 ~/.cache/AlbumArt.png 
 
 # Create notification
 notif=$(playerctl metadata --format "Title: {{ title }}\nArtist: {{ artist }}\nAlbum: {{ album }}")
-notify-send "Spotify" "$notif" --icon=~/.config/spotifyd/tmp/AlbumArt.png
+notify-send "Spotify" "$notif" --icon=~/.cache/AlbumArt.png
